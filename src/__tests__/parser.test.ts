d17abb057e01664253b65251909db4a5ae858552
Julian <liannsd3@gmail.com>
2023-07-28 17:14:42 -0300

    test for operator precedence

--- src/__tests__/parser.test.ts
+++ src/__tests__/parser.test.ts
@@ -362,3 +362,69 @@ test('Parsing infix expressions', () => {
     }
   }
 });
+
+test('Parsing operator precedence', () => {
+  let testCases: {
+    input: string;
+    expected: string;
+  }[] = [
+    {
+      input: '-a * b;',
+      expected: '((-a) * b)',
+    },
+    {
+      input: '!-a;',
+      expected: '(!(-a))',
+    },
+    {
+      input: 'a + b + c;',
+      expected: '((a + b) + c)',
+    },
+    {
+      input: 'a * b * c;',
+      expected: '((a * b) * c)',
+    },
+    {
+      input: 'a * b / c;',
+      expected: '((a * b) / c)',
+    },
+    {
+      input: 'a + b / c;',
+      expected: '(a + (b / c))',
+    },
+    {
+      input: 'a + b * c + d / e - f;',
+      expected: '(((a + (b * c)) + (d / e)) - f)',
+    },
+    {
+      input: '3 + 4; -5 * 5;',
+      expected: '(3 + 4)((-5) * 5)',
+    },
+    {
+      input: '5 > 4 == 3 < 4;',
+      expected: '((5 > 4) == (3 < 4))',
+    },
+    {
+      input: '5 < 4 != 3 > 4;',
+      expected: '((5 < 4) != (3 > 4))',
+    },
+    {
+      input: '3 + 4 * 5 == 3 * 1 + 4 * 5;',
+      expected: '((3 + (4 * 5)) == ((3 * 1) + (4 * 5)))',
+    },
+    {
+      input: '3 + 4 * 5 == 3 * 1 + 4 * 5;',
+      expected: '((3 + (4 * 5)) == ((3 * 1) + (4 * 5)))',
+    },
+  ];
+  testCases.forEach((test) => {
+    let lex = new lexer.Lexer(test.input);
+    let pars = new parser.Parser(lex);
+    let program = pars.parseProgram();
+    checkParserErrors(pars);
+
+    let actual = program.toString();
+
+    expect(actual).toEqual(test.expected);
+  });
+});
